25486376dc2c681616cfca9efae272a5ce8dc7a7
SelpaRizqi13 <selparizqi@gmail.com>
2021-12-02 01:04:29 -0800

    terhubung dengan API

--- src/Map.js
+++ src/Map.js
@@ -2,6 +2,7 @@ import React from "react";
 import L from "leaflet";
 import statesData from './data'
 import './map.css'
+import budaya from './image/budaya.jpg'
 
 const style = {
   width: "100vw",
@@ -19,165 +20,73 @@ const mapStyle = (feature) => {
   });
 }
 
+
 const getColor = (d) =>{
-  return d > 3201
-  ? "#800026"
-  : d > 3202
+  return d === 3201
+  ? "#80002"
+  : d === 3202
     ? "#BD0026"
-    : d > 3203
-      ? "#E31A1C"
-      : d > 3204
+    : d === 3203
+      ? "#E31A1C" 
+      : d === 3204
         ? "#FC4E2A"
-        : d > 3205
-          ? "#FD8D3C"
-		  : d > 3206
-          ? "#FD8D3C"
-		  : d > 3207
-          ? "#FD8D3C"
-		  : d > 3208
-          ? "#FD8D3C"
-		  : d > 3209
-          ? "#FD8D3C"
-		  : d > 3210
-          ? "#FD8D3C"
-		  : d > 3211
-          ? "#FD8D3C"
-		  : d > 3212
-          ? "#FD8D3C"
-		  : d > 3213
-          ? "#FD8D3C"
-		  : d > 3214
-          ? "#FD8D3C"
-		  : d > 3215
-          ? "#FD8D3C"
-		  : d > 3216
-          ? "#FD8D3C"
-		  : d > 3217
-          ? "#FD8D3C"
-		  : d > 3218
-          ? "#FD8D3C"
-		  : d > 3219
-          ? "#FD8D3C"
-		    : d > 3220
-          ? "#FD8D3C"
-		  : d > 3221
-          ? "#FD8D3C"
-		  : d > 3222
-          ? "#FD8D3C"
-		  : d > 3223
-          ? "#BD0026"
-		  : d > 3224
-      
-          ? "#FD8D3C"
-		  : d > 3225
-          ? "#FD8D3C"
-		  : d > 3226
-          ? "#FD8D3C"
-		  : d > 3227
-          ? "#FD8D3C"
-		  : d > 3228
-          ? "#FD8D3C"
-		  : d > 3229
-          ? "#FD8D3C"
-		  : d > 3230
-          ? "#FD8D3C"
-		  : d > 3231
-          ? "#FD8D3C"
-		  : d > 3232
-          ? "#FD8D3C"
-		  : d > 3233
-          ? "#FD8D3C"
-		  : d > 3234
-          ? "#FD8D3C"
-		  : d > 3235
-          ? "#FD8D3C"
-		  : d > 3236
-          ? "#FD8D3C"
-		  : d > 3237
-          ? "#FD8D3C"
-		  : d > 3238
-          ? "#FD8D3C"
-		  : d > 3239
-          ? "#FD8D3C"
-		  : d > 3240
-          ? "#FD8D3C"
-		  : d > 3241
-          ? "#FD8D3C"
-		  : d > 3242
-          ? "#FD8D3C"
-		  : d > 3243
-          ? "#FD8D3C"
-		  : d > 3244
-          ? "#FD8D3C"
-		  : d > 3245
-          ? "#FD8D3C"
-		  : d > 3246
-          ? "#FD8D3C"
-		  : d > 3247
-          ? "#FD8D3C"
-		  : d > 3248
-          ? "#FD8D3C"
-		  : d > 3249
-          ? "#FD8D3C"
-		  : d > 3250
-          ? "#FD8D3C"
-		  : d > 3251
-          ? "#FD8D3C"
-		  : d > 3252
-          ? "#FD8D3C"
-		  : d > 3253
-          ? "#FD8D3C"
-		  : d > 3254
-          ? "#FD8D3C"
-		  : d > 3255
-          ? "#FD8D3C"
-		  : d > 3256
-          ? "#FD8D3C"
-		  : d > 3257
-          ? "#FD8D3C"
-		  : d > 3258
-          ? "#FD8D3C"
-		  : d > 3259
-          ? "#FD8D3C"
-		  : d > 3260
-          ? "#FD8D3C"
-		  : d > 3261
-          ? "#FD8D3C"
-		  : d > 3262
-          ? "#FD8D3C"
-		  : d > 3263
-          ? "#FD8D3C"
-		  : d > 3264
-          ? "#FD8D3C"
-		  : d > 3265
-          ? "#FD8D3C"
-		  : d > 3266
-          ? "#FD8D3C"
-		  : d > 3267
-          ? "#FD8D3C"
-		  : d > 3268
-          ? "#FD8D3C"
-		  : d > 3270
-          ? "#FD8D3C"
-		  : d > 3271
-          ? "#FD8D3C"
-		  : d > 3272
-          ? "#FD8D3C"
-		  : d > 3273
-          ? "#FD8D3C"
-		  : d > 3274
-          ? "#FD8D3C"
-		  : d > 3275
-          ? "#FD8D3C"
-		  : d > 3276
-          ? "#FD8D3C"
-		  : d > 3277
-          ? "#FD8D3C"
-          : d > 3278 ? "#FEB24C" : d > 3279 ? "#FED976" : "#FFEDA0";
+        : d === 3205
+          ? "#d3fc03"
+		  : d === 3206
+          ? "#45fc03"
+		  : d === 3207
+          ? "#03fcfc"
+		  : d === 3208
+          ? "#03c6fc"
+		  : d === 3209
+          ? "#038cfc"
+		  : d === 3210
+          ? "#035efc"
+		  : d === 3211
+          ? "#031cfc"
+		  : d === 3212
+          ? "#6203fc"
+		  : d === 3213
+          ? "#8403fc"
+		  : d === 3214
+          ? "#a903fc"
+		  : d === 3215
+          ? "#fc03ce"
+		  : d === 3216
+          ? "#fc0390"
+		  : d === 3217
+          ? "#fc034a"
+		  : d === 3218
+          ? "#fc0331"
+		  : d === 3271
+          ? "#fc6703"
+		  : d === 3272
+          ? "#c7735a"
+		  : d === 3273
+          ? "#c79a5a"
+		  : d === 3274
+          ? "#c7af5a"
+		  : d === 3275
+          ? "#9bc75a"
+		  : d === 3276
+          ? "#5ac787"
+		  : d === 3277
+          ? "#5ab7c7"
+          : d === 3278 ? "#FEB24C" : d === 3279 ? "#FED976" : "#FFEDA0";
 }
 
 class Map extends React.Component {
-  componentDidMount() {
+  state ={
+    loading : true,
+    person : null,
+  };
+  
+  async componentDidMount() {
+    const url = "http://127.0.0.1:8000/api/getDaerah";
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+
     // create map
     this.map = L.map("map", {
       center: [	-6.914744, 107.609810],
@@ -192,6 +101,22 @@ class Map extends React.Component {
         })
       ]
     });
+
+    let template =`
+    <h1>8 Unsur Budaya</h1> 
+    <img src={budaya} /> </br>
+    <a href="">1. Religi</a></br>
+    <a href="">2. Kekerabatan dan Organisasi Sosial</a> </br>
+    <a href="">3. Bahasa</a></br>
+    <a href="">4. Kesenian</a></br>
+    <a href="">5. Teknologi</a></br>
+    <a href="">6. Ekonomi</a></br>
+    <a href="">7. Pengetahuan</a></br>
+    <a href="">8. Makanan</a></br>
+    `
+
+    L.marker([-6.551776, 106.629128]).addTo(this.map)
+    .bindPopup(template);
 
     this.geojson = L.geoJson(statesData, {
       style: mapStyle,
@@ -249,7 +174,25 @@ class Map extends React.Component {
     this.map.fitBounds(e.target.getBounds());
   }
   render() {
-    return <div id="map" style={style} />;
+    var {items} = this.state;
+    return (
+    <div id="map" style={style}> 
+    {
+      this.state.loading || !this.state.person ? (
+        <div>
+          loading ...
+        </div>
+      ) :
+      (
+        <div> 
+          <div>
+            {this.state.person.name_area}
+          </div>
+        </div>
+      )
+    }
+    </div>
+    );
   }
 }
 
